6b7929657b593fabf203eb9f203d19cd54d83ba0
Francisco Duarte <francisco.caramez.duarte@gmail.com>
2022-10-23 22:51:08 +0100

    finishes data fetching section with json-server

--- router-project/src/views/jobs/JobDetailsView.vue
+++ router-project/src/views/jobs/JobDetailsView.vue
@@ -1,16 +1,32 @@
 <template>
   <h1>Job Description</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>
+      {{ job.title }}
+    </h1>
+    <p>
+      {{ job.details }}
+    </p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
-  /*  data() {
+  data() {
     return {
-      id: this.$route.params.id,
+      job: null,
     };
-  }, */
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.error(err.message));
+  },
 };
 </script>
 
