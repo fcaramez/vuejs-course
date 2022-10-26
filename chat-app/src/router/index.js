54b3bd65c4cb469dfa9aebc93f9f77843e81f1f1
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-26 10:18:20 +0100

    finishes authGuard

--- chat-app/src/router/index.js
+++ chat-app/src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Welcome from "../views/WelcomeView.vue";
 import ChatRoom from "../views/ChatRoomView.vue";
+import { projectAuth } from "@/firebase/config";
+
+// auth guard
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (!user) {
+    next({ name: "welcome" });
+  } else {
+    next();
+  }
+};
 
 const routes = [
   {
@@ -12,6 +23,7 @@ const routes = [
     path: "/chatroom",
     name: "chatroom",
     component: ChatRoom,
+    beforeEnter: requireAuth,
   },
 ];
 
